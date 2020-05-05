db4f3e37629264c66ece37ddb28da7daf2d65fe3
Kyle Rector <kylearector@outlook.com>
2020-05-05 13:26:37 -0400

    Making helper function

--- lambda_function.py
+++ lambda_function.py
@@ -14,35 +14,31 @@ STATUS_OPTIONS = [
   ("Commuting", ":car:")
 ]
 
+def update_slack_status(status, emoji):
+  clients = [slack.WebClient(val) for key, val in os.environ.items() if "slack_token" in key]
+  for client in clients:
+    client.users_profile_set(
+      profile={
+          "status_text": status,
+          "status_emoji": emoji
+        }
+    )
+
 def lambda_handler(event, context):
   if "status" in event["data"]:
-    if event["data"]["status"] in dict(STATUS_OPTIONS):
+    if event["data"]["status"] in dict(STATUS_OPTIONS) or event["data"]["status"] == "Clear":
       status = ""
       emoji = ""
       if event["data"]["status"] != "Clear":
         status = event["data"]["status"]
         emoji = dict(STATUS_OPTIONS)[status]
-      clients = [slack.WebClient(val) for key, val in os.environ.items() if "slack_token" in key]
-      for client in clients:
-        client.users_profile_set(
-          profile={
-              "status_text": status,
-              "status_emoji": emoji
-            }
-        )
+      update_slack_status(status, emoji)
     else:
       raise KeyError("The requested status is not in the list of statuses")
   elif "index" in event["data"]:
     pos = event["data"]["index"]
     if 0 <= pos < len(STATUS_OPTIONS):
-      clients = [slack.WebClient(val) for key, val in os.environ.items() if "slack_token" in key]
-      for client in clients:
-        client.users_profile_set(
-          profile={
-              "status_text": STATUS_OPTIONS[pos][0],
-              "status_emoji": STATUS_OPTIONS[pos][1]
-            }
-        )
+      update_slack_status(STATUS_OPTIONS[pos][0], STATUS_OPTIONS[pos][1])
     else:
       raise IndexError("The requested status index is out of range.")
   else:
